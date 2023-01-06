c629e56f675da655396b09f6ac83e06611436c5f
Aliaksei Shkurko <shaleprog@gmail.com>
2023-01-06 15:31:41 +0300

    init commit

---
+++ src/main/java/by/x1ss/adapterservice/AdapterServiceApplication.java
@@ -0,0 +1,13 @@
+package by.x1ss.adapterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdapterServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/x1ss/adapterservice/AdapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.x1ss.adapterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
