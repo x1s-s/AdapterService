8aa68af22dd9dc4bd5db12d0b9e6e0060bdeac75
Aliaksei Shkurko <shaleprog@gmail.com>
2023-01-07 15:47:46 +0300

    add restTemplate bean

---
+++ src/main/java/by/x1ss/adapterservice/config/AdapterConfiguration.java
@@ -0,0 +1,13 @@
+package by.x1ss.adapterservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AdapterConfiguration {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
